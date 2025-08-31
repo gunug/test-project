7fabd54faa15f5211e60974408dbe0eb02d05868
GunaJona <gunug@naver.com>
2025-08-31 13:02:03 +0900

    document update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React, {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
   function test1(){
     return 1000;
   }
+  function test2():number{
+    b('바지');
+    d(['아이템1','아이템2','아이템3']);
+          return 100;
+        }
   let [a,b] = useState('코트'); //[state 데이터, state 데이터 변경 함수]
   let [c,d] = useState(['변수1','변수2','변수3']);
   let [counter, setCounter] = useState(0);
@@ -17,7 +19,9 @@ function App() {
       <div className="test">Hello</div>
       <div onClick={() => alert('안녕')}>Hello</div>
       test-project
-      { count }
+      {
+        test2()
+      }
       {test1()}
       { a }
       {c}{c[1]}
---
+++ src/Test.tsx
@@ -0,0 +1,8 @@
+function Test() {
+    return (
+       <div className="App">
+        test
+        </div>
+    )
+}
+export default Test;
